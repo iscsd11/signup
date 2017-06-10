bcd6326e5c7be3cd1eecdc01ff21b3021f6d778c
iscsd11 <iscsd11@ems.tcust.edu.tw>
2017-06-10 16:48:58 +0800

    逐步修改

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">主控台</div>
+
+                <div class="panel-body">
+                    您已經登入囉！
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
